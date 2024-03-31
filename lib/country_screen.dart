3aaf30c27a8b413440c45000169ee192f0dfae61
dimash0 <dimash0001@gmail.com>
2024-03-31 18:12:26 +0600

    Изменил текст страна

--- lib/country_screen.dart
+++ lib/country_screen.dart
@@ -43,7 +43,7 @@ class _CountryScreenState extends State<CountryScreen> {
       appBar: AppBar(
           backgroundColor: Color.fromRGBO(34, 43, 72, 1),
           title: Text(
-            'Список стран',
+            'Список Cтран',
             style: TextStyle(fontSize: 25, color: Colors.white),
           )),
       body: ListView.builder(
